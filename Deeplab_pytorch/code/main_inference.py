5399558d629b5725560bd750e4948d14e1b0604e
Laura Martinez Sanchez <lmartisa@gmail.com>
2021-05-26 21:32:22 +0200

    Add files via upload

---
+++ Deeplab_pytorch/code/main_inference.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Author: Kazuto Nakashima
+# URL:    https://kazuto1011.github.io
+# Date:   07 January 2019
+
+from __future__ import absolute_import, division, print_function
+
+import cv2
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import yaml
+from addict import Dict
+from PIL import Image
+import os
+
+from libs.models import *
+from libs.utils import DenseCRF
+import time
+
+
+def get_device(cuda):
+    cuda = cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if cuda else "cpu")
+    if cuda:
+        current_device = torch.cuda.current_device()
+        print("Device:", torch.cuda.get_device_name(current_device))
+    else:
+        print("Device: CPU")
+    return device
+
+
+def get_classtable(CONFIG):
+    with open(CONFIG.DATASET.LABELS) as f:
+        classes = {}
+        for label in f:
+            label = label.rstrip().split("\t")
+            classes[int(label[0])] = label[1].split(",")[0]
+    return classes
+
+
+def setup_postprocessor(CONFIG):
+    # CRF post-processor
+    postprocessor = DenseCRF(
+        iter_max=CONFIG.CRF.ITER_MAX,
+        pos_xy_std=CONFIG.CRF.POS_XY_STD,
+        pos_w=CONFIG.CRF.POS_W,
+        bi_xy_std=CONFIG.CRF.BI_XY_STD,
+        bi_rgb_std=CONFIG.CRF.BI_RGB_STD,
+        bi_w=CONFIG.CRF.BI_W,
+    )
+    return postprocessor
+
+def preprocessing(image, device, CONFIG):
+    # Resize
+    scale = CONFIG.IMAGE.SIZE.TEST / max(image.shape[:2])
+    image = cv2.resize(image, dsize=None, fx=scale, fy=scale)
+    raw_image = image.astype(np.uint8)
+
+    # Subtract mean values
+    image = image.astype(np.float32)
+    mean_v = np.mean(image, axis=(0,1))
+    print(mean_v)
+    image -= np.array(
+        [
+            float(mean_v[0]),
+            float(mean_v[1]),
+            float(mean_v[2]),
+        ]
+    )
+
+    # Convert to torch.Tensor and add "batch" axis
+    image = torch.from_numpy(image.transpose(2, 0, 1)).float().unsqueeze(0)
+    image = image.to(device)
+
+    return image, raw_image
+
+
+def inference(model, image, raw_image=None, postprocessor=None):
+    _, _, H, W = image.shape
+
+    # Image -> Probability map
+    logits = model(image)
+    logits = F.interpolate(logits, size=(H, W), mode="bilinear", align_corners=False)
+    probs = F.softmax(logits, dim=1)[0]
+    probs = probs.cpu().numpy()
+
+    # Refine the prob map with CRF
+    if postprocessor and raw_image is not None:
+        probs = postprocessor(raw_image, probs)
+
+    labelmap = np.argmax(probs, axis=0)
+
+    return labelmap
+
+def save_image(npdata,outfilename):
+    img = Image.fromarray(np.asarray(np.clip(npdata, 0, 255), dtype="uint8"), "L" )
+    img.save(outfilename, compress_level = 0)
+
+
+def main(ctx):
+    """
+    Demo with a trained model
+    """
+
+    print("Mode:")
+
+
+
+def single(config_path, model_path, image_path, output_path,  cuda, crf):
+  """Inference"""
+  with open(config_path, 'r') as stream:
+      aux = yaml.load(stream)
+  CONFIG = Dict(aux)
+  print(CONFIG)
+  device = get_device(cuda)
+  torch.set_grad_enabled(False)
+
+  classes = get_classtable(CONFIG)
+  postprocessor = setup_postprocessor(CONFIG) if crf else None
+  model = eval(CONFIG.MODEL.NAME)(n_classes=CONFIG.DATASET.N_CLASSES)
+  state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)
+  model.load_state_dict(state_dict)
+  model.eval()
+  model.to(device)
+  print("Model:", CONFIG.MODEL.NAME)
+
+  # Inference
+  image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+  gamma = 0.3
+  lookUpTable = np.empty((1,256), np.uint8)
+  for i in range(256):
+      lookUpTable[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+  image = cv2.LUT(image, lookUpTable)
+  image, raw_image = preprocessing(image, device, CONFIG)
+  labelmap = inference(model, image, raw_image, postprocessor)
+
+  labels, counts = np.unique(labelmap, return_counts=True)
+  ## Show result for each class
+  rows = np.floor(1)
+  cols = np.ceil(2)
+  plt.figure(figsize=(10, 10))
+  ax = plt.subplot(rows, cols, 1)
+  ax.set_title("Input image")
+  ax.imshow(raw_image[:, :, ::-1])
+  ax.axis("off")
+  for i, label in enumerate(labels):
+    #if label == 16 or label == 17:
+    if label == 8 or label == 12:
+        mask = labelmap == label
+        ax = plt.subplot(rows, cols, 2)
+        ax.set_title(classes[label])
+        ax.imshow(raw_image[..., ::-1])
+        ax.imshow(mask.astype(np.float32), alpha=0.5)
+        ax.axis("off")
+  plt.tight_layout()
+  plt.show(block=True)
+
